1c1871d93f3ed77d1b6cf3d1b57a61ddcd887824
John Peterson <peterson.glock17@gmail.com>
2021-11-20 15:06:47 -0700

    Added font awesome. Fixed docker node_modules issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-// TODO: Add Font awesome
+
+// Font Awesome Imports
+import '@fortawesome/fontawesome-free';
 
 import './index.css';
 import App from './App';
